46d889f038f5bd969fb0e19e27704b3a1bf4407b
Michal Zygor <michal@winami.com>
2020-10-15 21:36:19 +0200

    005_ex_1: correction. Added structure "else" and non alcoholic option. In this case if..else is more clear in use.

--- src/Bar.java
+++ src/Bar.java
@@ -1,13 +1,20 @@
 public class Bar {
     public static void main(String[] args) {
         Person person1 = new Person("Tomasz", "Banaś", 19);
+        Person person2 = new Person("Walker", "Bieniasz", 16);
+        Person person3 = new Person("Franek", "Tylka", 15);
+
         Drink drink1 = new Drink("Blue Mary", 45.44, true);
+        Drink drink2 = new Drink("Ajerkoniak", 20.15, true);
+        Drink drink3 = new Drink("Lemoniada", 10.33, false);
+
         Barman barman = new Barman();
         barman.order(person1, drink1);
 
-        Person person2 = new Person("Walker", "Bieniasz", 16);
-        Drink drink2 = new Drink("Ajerkoniak", 20.15, true);
         Barman barman1 = new Barman();
         barman.order(person2, drink2);
+
+        Barman barman2 = new Barman();
+        barman.order(person3, drink3);
     }
 }
--- src/Barman.java
+++ src/Barman.java
@@ -1,11 +1,15 @@
 public class Barman {
 
     public void order(Person person, Drink drink) {
-        if(person.getAge() < 18 && drink.isAlcohol())
+        if(!drink.isAlcohol())
+            System.out.println(person.getName() + ". Twój napój bezalkoholowy jest gotowy do odbioru. "
+                    + "Kwota do zapłaty to " + drink.getPrice());
+        else if(person.getAge() < 18 && drink.isAlcohol())
             System.out.println("Odmowa sprzedazy. Drink zawiera alkohol");
-        if (person.getAge() >= 18) {
+        else if (person.getAge() >= 18) {
             System.out.println(person.getName() + ". Twój drink jest gotowy do odbioru. Kwota do zapłaty to "
                     + drink.getPrice());
         }
+
     }
 }
